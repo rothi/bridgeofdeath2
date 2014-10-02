3b584d158eb79aea32d640bbaec66c676caa9d41
rothi <rothi@pagosa.k12.co.us>
2014-10-02 10:03:15 -0600

    bridgeofdeath2
    Second bridge assignment

---
+++ bridgeofdeath2/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnApproach = New System.Windows.Forms.Button()
+        Me.lblAnswer1 = New System.Windows.Forms.Label()
+        Me.lblAnswer3 = New System.Windows.Forms.Label()
+        Me.lblAnswer2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnRestart = New System.Windows.Forms.Button()
+        Me.lblFinal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnApproach
+        '
+        Me.btnApproach.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnApproach.Location = New System.Drawing.Point(12, 12)
+        Me.btnApproach.Name = "btnApproach"
+        Me.btnApproach.Size = New System.Drawing.Size(226, 35)
+        Me.btnApproach.TabIndex = 0
+        Me.btnApproach.Text = "Approach the Bridge"
+        Me.btnApproach.UseVisualStyleBackColor = True
+        '
+        'lblAnswer1
+        '
+        Me.lblAnswer1.AutoSize = True
+        Me.lblAnswer1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer1.Location = New System.Drawing.Point(47, 50)
+        Me.lblAnswer1.Name = "lblAnswer1"
+        Me.lblAnswer1.Size = New System.Drawing.Size(0, 25)
+        Me.lblAnswer1.TabIndex = 1
+        '
+        'lblAnswer3
+        '
+        Me.lblAnswer3.AutoSize = True
+        Me.lblAnswer3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer3.Location = New System.Drawing.Point(47, 96)
+        Me.lblAnswer3.Name = "lblAnswer3"
+        Me.lblAnswer3.Size = New System.Drawing.Size(0, 25)
+        Me.lblAnswer3.TabIndex = 2
+        '
+        'lblAnswer2
+        '
+        Me.lblAnswer2.AutoSize = True
+        Me.lblAnswer2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAnswer2.Location = New System.Drawing.Point(47, 74)
+        Me.lblAnswer2.Name = "lblAnswer2"
+        Me.lblAnswer2.Size = New System.Drawing.Size(0, 25)
+        Me.lblAnswer2.TabIndex = 3
+        Me.lblAnswer2.UseWaitCursor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 100)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 25)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Q3:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(7, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(46, 25)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Q2:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(7, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 25)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Q1:"
+        '
+        'btnRestart
+        '
+        Me.btnRestart.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRestart.Location = New System.Drawing.Point(244, 12)
+        Me.btnRestart.Name = "btnRestart"
+        Me.btnRestart.Size = New System.Drawing.Size(93, 35)
+        Me.btnRestart.TabIndex = 7
+        Me.btnRestart.Text = "Restart"
+        Me.btnRestart.UseVisualStyleBackColor = True
+        '
+        'lblFinal
+        '
+        Me.lblFinal.AutoSize = True
+        Me.lblFinal.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFinal.Location = New System.Drawing.Point(13, 129)
+        Me.lblFinal.Name = "lblFinal"
+        Me.lblFinal.Size = New System.Drawing.Size(0, 25)
+        Me.lblFinal.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(573, 389)
+        Me.Controls.Add(Me.lblFinal)
+        Me.Controls.Add(Me.btnRestart)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblAnswer2)
+        Me.Controls.Add(Me.lblAnswer3)
+        Me.Controls.Add(Me.lblAnswer1)
+        Me.Controls.Add(Me.btnApproach)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnApproach As System.Windows.Forms.Button
+    Friend WithEvents lblAnswer1 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer3 As System.Windows.Forms.Label
+    Friend WithEvents lblAnswer2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnRestart As System.Windows.Forms.Button
+    Friend WithEvents lblFinal As System.Windows.Forms.Label
+
+End Class
---
+++ bridgeofdeath2/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+
+    Private Sub btnApproach_Click(sender As Object, e As EventArgs) Handles btnApproach.Click
+        Dim strName As String = InputBox("What is your name?", "Name")
+        Dim strQuest As String = InputBox("What is your quest?", "Name")
+        Dim intRandomQuestionNumber As Integer
+        Dim strRandomQAnswer As String
+
+        Dim strRandomQ1 As String = "What is the flight velocity of an unlaiden swallow?"
+        Dim strRandomQ2 As String = "What is your favorite color?"
+        Dim strRandomQ3 As String = "How is your day?"
+        Dim strRandomQ4 As String = "What is the meaining of life?"
+
+        Randomize()
+        intRandomQuestionNumber = Int((4 - 1 + 1) * Rnd() + 1)
+
+        If intRandomQuestionNumber = 1 Then
+            strRandomQAnswer = InputBox(strRandomQ1, "random")
+        ElseIf intRandomQuestionNumber = 2 Then
+            strRandomQAnswer = InputBox(strRandomQ2, "random")
+        ElseIf intRandomQuestionNumber = 3 Then
+            strRandomQAnswer = InputBox(strRandomQ3, "random")
+        ElseIf intRandomQuestionNumber = 4 Then
+            strRandomQAnswer = InputBox(strRandomQ4, "random")
+        End If
+
+        If strName = "Ian" Then
+            Me.lblAnswer1.Text = "Good"
+        ElseIf strName <> "Ian" Then
+            Me.lblAnswer1.Text = "You die."
+        End If
+
+        If strQuest = "To seek the Holy Grail" Then
+            Me.lblAnswer2.Text = "Good"
+        ElseIf strQuest <> "To seek the Holy Grail" Then
+            Me.lblAnswer2.Text = "You die."
+        End If
+
+        If intRandomQuestionNumber = 1 And strRandomQAnswer = "52.5mph" Then
+            Me.lblAnswer3.Text = "Good"
+        ElseIf intRandomQuestionNumber = 1 And strRandomQAnswer <> "52.5mph" Then
+            Me.lblAnswer3.Text = "You die."
+        End If
+        If intRandomQuestionNumber = 2 And strRandomQAnswer = "Blue" Then
+            Me.lblAnswer3.Text = "Good"
+        ElseIf intRandomQuestionNumber = 2 And strRandomQAnswer <> "Blue" Then
+            Me.lblAnswer3.Text = "You die."
+        End If
+        If intRandomQuestionNumber = 3 And strRandomQAnswer = "Good, thanks!" Then
+            Me.lblAnswer3.Text = "Good"
+        ElseIf intRandomQuestionNumber = 3 And strRandomQAnswer <> "Good, thanks!" Then
+            Me.lblAnswer3.Text = "You die."
+        End If
+        If intRandomQuestionNumber = 4 And strRandomQAnswer = "To live every day to it's fullest" Then
+            Me.lblAnswer3.Text = "Good"
+        ElseIf intRandomQuestionNumber = 4 And strRandomQAnswer <> "To live every day to it's fullest" Then
+            Me.lblAnswer3.Text = "You die."
+        End If
+
+        If Me.lblAnswer1.Text = "Good" And Me.lblAnswer2.Text = "Good" And Me.lblAnswer3.Text = "Good" Then
+            Me.lblFinal.Text = "All 3 are correct. YOU MAY PASS!"
+        Else : Me.lblFinal.Text = "Some answers are incorrect. YOU DIE!"
+        End If
+    End Sub
+
+    Private Sub btnRestart_Click(sender As Object, e As EventArgs) Handles btnRestart.Click
+        Me.lblAnswer1.Text = ""
+        Me.lblAnswer2.Text = ""
+        Me.lblAnswer3.Text = ""
+        Me.lblFinal.Text = ""
+    End Sub
+End Class
